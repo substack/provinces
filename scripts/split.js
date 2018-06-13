2c7ea7a14809c3ac71940352508af2a65a64b4f2
Daniel Holzmann <d@velopment.at>
2018-06-13 12:29:30 +0200

    add list of available countries

--- scripts/split.js
+++ scripts/split.js
@@ -11,7 +11,10 @@ if (!existsSync(outputFolder)) {
 }
 
 function writeCountry(countryCode, provinces) {
-    const countryFile = resolve(outputFolder, `${countryCode.toLowerCase()}.json`);
+    const countryFile = resolve(
+        outputFolder, 
+        `${countryCode.toLowerCase()}.json`,
+    );
     writeFileSync(countryFile, JSON.stringify(provinces));
     console.log(`exported ${provinces.length} provinces to ${countryFile}`);
 }
@@ -24,4 +27,13 @@ const countriesMap = data.reduce((result, {country, ...entry}) => ({
     [country]: [...(result[country] || []), entry]
 }), {});
 
-Object.keys(countriesMap).forEach(country => writeCountry(country, countriesMap[country]))
+
+Object
+    .keys(countriesMap)
+    .forEach(country => writeCountry(country, countriesMap[country]));
+
+// add file with a list of countries
+const countryCodes = Object.keys(countriesMap)
+    .map(countryCode => countryCode.toLowerCase());
+const listFile = resolve(__dirname, '..', 'countries.json');
+writeFileSync(listFile, JSON.stringify(countryCodes));
